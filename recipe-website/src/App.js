51643d5aa5df4e350744660e0d4351d50834a1a4
bedrantirak <bdrntrk@gmail.com>
2020-10-22 10:00:56 +0300

    Update App.js

--- recipe-website/src/App.js
+++ recipe-website/src/App.js
@@ -1,13 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useEffect,useState} from 'react';
 import './App.css';
 
-function App() {
-  return (
+const App = () =>{
+  const APP_ID = "bd4e6bc4";
+  const APP_KEY = "92af103e6c8de6672a6c4dc1f2bdc0fd";
+
+  const [recipes, setRecipes] =useState([]);
+
+  useEffect(()=>{
+
+    getRecipes();
+  },[]);
+  
+  const getRecipes = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
+    const data =await response.json();
+       setRecipes(data.hits);
+  };
+  return(
     <div className="App">
-     
+      <form  className="search-form">
+        <input type="text" className="search-bar"/>
+        <button className="search-button">Search</button>
+      </form>
     </div>
   );
-}
+
+
+
+};
 
 export default App;
